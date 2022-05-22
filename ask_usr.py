a18a48db231a255927286080fb8f0e3faa23afaa
Erick Giraldo <santiagogiraldo_514@hotmail.com>
2022-05-21 19:39:55 -0500

    finshd func 4 asking time in the correct format

--- ask_usr.py
+++ ask_usr.py
@@ -1,5 +1,6 @@
 from datetime import *
-import re
+import pytz
+from dateutil.parser import parse
 
 def main_menu():
     ans = '0'
@@ -21,23 +22,69 @@ def ask_minutes():
         try:
             ans = int(ans)
         except:
-            print('Please input a valid answer')
+            print('Enter a valid answer')
             continue
         else:
             if ans < 0 or ans > 5:
-                print('Please input a number between 0 and 5')
+                print('Enter a number between 0 and 5')
                 continue
 
         break
 
     return ans
 
-def ask_datetime():
-    # Check the correct format is entered (With re maybe?)
-    # Check whether the datetime is later than the current time
-    now = now
+def ask_timezone():
+    # For the option in which the user chooses to set the time, it must be asked first what is their timezone
+    # Ask for showing the user the supported timezones
     pass
 
+def ask_datetime():
+    print('------ALARM------')
+    print('Supported formats for the time: "hh:mm" or "h:mm" (Military time)')
+    # For practicity, only hours and minutes are allowed. This hours and minutes are parsed as being in the same day
+    while True:
+        ans = input('Please input the time: ')
+
+        if (len(ans) < 4 or len(ans) > 5) or ':' not in ans:
+            print('Enter a valid time')
+            continue
+
+        hour, _, minute = ans.partition(':')
+        if (len(hour) < 1 or len(hour) > 2) or (len(minute) < 1 or len(minute) > 2):
+            print('Formats other than "hh:mm" or "h:mm" are not accepted.')
+            continue
+        elif len(hour) == 2 and len(minute) == 1:   # For rejecting format "hh:m"
+            print('Format "hh:m" not allowed. Enter the time in the correct format ("hh:mm", "h:mm").')
+            continue
+
+        try:
+            hour = int(hour)
+            minute = int(minute)
+        except:
+            print('Enter only integer numbers for the time.')
+            continue
+        else:
+            if hour < 0 or hour > 23:
+                print('Only numbers between 0 and 23 can be entered for the hours')
+            elif minute < 0 or minute > 59:
+                print('Only numbers between 0 and 59 can be entered for the minutes')
+            else:
+                # Time to check the time given by the user is not 'lesser' than the current time
+                timezone = pytz.timezone('America/Bogota') # Default timezone
+                now = datetime.now() # Current time
+                now_loc = timezone.localize(now)
+                usr_time = str(hour)+':'+str(minute)
+                usr_time = parse(usr_time)
+                usr_time_loc = timezone.localize(usr_time)
+                if usr_time_loc < now_loc:
+                    print('The entered time must be "greater" the the current time.')
+                    continue
+
+                break # The time is well formatted
+
+    return usr_time
+
+
 if __name__ == '__main__':
-    ans = ask_minutes()
-    print('The minutes are: ', ans)
+    ans = ask_datetime()
+    print('The datetime set for your alarm is: ', ans)
